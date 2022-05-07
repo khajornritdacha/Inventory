64c865addcf8df1bb2ebb171f840aa96e70edf76
khajornritdacha <8501582+khajornritdacha@users.noreply.github.com>
2022-05-07 18:35:47 +0700

    finish body parser

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 
 class Items {
@@ -10,15 +13,24 @@ class Items {
     }
 }
 
+let data = [];
+data.push(new Items("Key", "Room1"));
+data.push(new Items("Tissue", "RoomX"));
+data.push(new Items("Soap", "Toilet"));
+data.push(new Items("Shampoo", "Toilet"));
 
 app.get("/", (req, res) => {
-    let data = [];
-    data.push(new Items("Key", "Room1"));
-    data.push(new Items("Tissue", "RoomX"));
-    data.push(new Items("Soap", "Toilet"));
-    data.push(new Items("Shampoo", "Toilet"));
     res.render("index", {items: data})
 });    
+
+app.get("/add", (req, res) => {
+    res.render("add"); 
+});
+
+app.post("/add", (req, res) => {
+    console.log(req.body);
+    res.send("Complete");
+});
 
 
 app.listen(3000, () => {console.log("Server is running at port 3000");});
